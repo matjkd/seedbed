01d502878de3f2824f90d0381e3b44bfa8254a4b
Mat Sadler <mat@redstudio.co.uk>
2012-07-05 08:25:16 +0000

    added driving directions

--- application/views/sidebox/contact_side.php
+++ application/views/sidebox/contact_side.php
@@ -1,2 +1,27 @@
 
  <div id="map_canvas" style="width:460px;height:400px;"></div>
+ <h3>How to find us.</h3>
+ <h4>By Train</h4>
+<strong>Our nearest station is Debden, on the Central line.</strong><br/>
+<p>Exit the station and walk down to the main road (Chigwell Lane) turn left, pass under
+the railway bridge until you reach the traffic lights at Langston Road. Turn left again
+we are about 400 metres down on the right. From the station to us it takes about 8
+minutes.</p>
+
+<h4>By Road</h4>
+<strong>M11</strong><br/>
+<p>Exit the M11 at junction 5 turn left off of the motorway exit Road into Chigwell Lane
+(A1168), drive 200 metres to the first set of traffic lights at Langston Road, turn right.
+We are 400 metres down on the right hand side.</p>
+<p>Unfortunately only traffic driving from London can exit at Junction 5. If you are
+coming to us from outside the M25 etc you will not be able to exit at junction 5.</p>
+
+<strong>M25</strong><br/>
+<p>Exit the M25 at junction 26 and take the A121 towards Loughton drive for just over
+a mile until you reach a roundabout (formally Wake Arms roundabout) take the 3rd
+exit staying on the A121 towards Loughton. After about 1 and ¼ miles you will reach
+2 mini roundabouts close together, at the 2nd mini roundabout turn left into Rectory
+Lane (A1168). Drive for just over a mile you will drive under Debden station railway
+bridge keep going a short distance until you reach the traffic lights. Turn left at the
+traffic lights into Langston Road, we are 400 metres down on the right. Journey time
+is around ten minutes in a car from the M25 exit.</p>
