466bef27867bf2d10d2cb304b9b45a27a69eda6a
Mat Sadler <mat@redstudio.co.uk>
2012-09-10 14:07:06 +0000

    added conference image to conference section

--- application/views/extra/mailbox/conferenceRoom.php
+++ application/views/extra/mailbox/conferenceRoom.php
@@ -2,7 +2,9 @@
         <div class="mainheading" ><h3>Conference Room</h3></div>
         <div class="alpha twelve columns confroom"  >  
             <div style="padding:5px;">
-               <p>Available for Meetings/Training or Interviews, and can accomodate up to 10 people.
+            
+            <img src="https://s3-eu-west-1.amazonaws.com/seedbed/IMG_7060_1_2_500.jpg" style="float:right; width:300px; margin-left:20px; padding-top:10px;"/>
+               <p><strong>Available for Meetings/Training or Interviews, and can accomodate up to 10 people.</strong>
                 </p>
                 
                 <p>Additional facilities include:
