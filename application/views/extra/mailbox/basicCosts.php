1093fe29b2ae86e59e8d650628b676e6a1453ffa
Mat Sadler <mat@redstudio.co.uk>
2015-07-02 10:17:31 +0100

    updated costs

--- application/views/extra/mailbox/basicCosts.php
+++ application/views/extra/mailbox/basicCosts.php
@@ -12,8 +12,7 @@
             <h4>Refundable deposits </h4>
             Level 1  £150.00 deposit is required.<br/>
             Level 2 £200.00 deposit is required. <br/>	
-            Level 3 £250.00 deposit is required. <br/>	
-            Level 4 £300.00 deposit is required. <br/>	
+         
             </p>
         </div>
 
