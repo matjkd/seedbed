cbe7c802eb6fe37e5e7a9210c13b4980bf786b69
Mat Sadler <mat@redstudio.co.uk>
2016-03-06 21:34:02 +0000

    changed downloads

--- application/views/template/elegant/extra/downloadterms.php
+++ application/views/template/elegant/extra/downloadterms.php
@@ -1,8 +1,8 @@
 <div class="container">	<!--this is not inside a .container div because it's not very long. You'll need to adjust the html and the css if you use this differently-->
 <section class="call-to-action">
-<a class="btn custom-btn btn-large btn-primary" target="_blank" href="https://s3-eu-west-1.amazonaws.com/seedbed/LSC+Standard+Licence+2014.pdf">Download Licence Agreement <i class="e-icon-download"></i></a>
+<a class="btn custom-btn btn-large btn-primary" target="_blank" href="https://s3-eu-west-1.amazonaws.com/seedbed/Licence+FOR+BROCHURE.pdf">Download Licence Agreement <i class="e-icon-download"></i></a>
 <a class="btn custom-btn btn-large btn-primary" target="_blank" href="https://s3-eu-west-1.amazonaws.com/seedbed/LSC+Estate+Regulations+2014.pdf">Download Estate Regulations <i class="e-icon-download"></i></a>
-<a class="btn custom-btn btn-large btn-primary" target="_blank" href="https://s3-eu-west-1.amazonaws.com/seedbed/tenant+application2014+pdf.pdf">Download Tenant Application <i class="e-icon-download"></i></a>
+<a class="btn custom-btn btn-large btn-primary" target="_blank" href="https://s3-eu-west-1.amazonaws.com/seedbed/tenant+application2016+pdf.pdf">Download Tenant Application <i class="e-icon-download"></i></a>
 
 </section><!--close .call-to-action-->
 </div>
